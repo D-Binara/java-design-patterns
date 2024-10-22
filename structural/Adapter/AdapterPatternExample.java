e09d8fe0727d4155e7a20221e1788d0b99e137a3
D-Binara <dbkaveesha@gmail.com>
2024-10-22 23:59:33 +0530

    create other patterns

---
+++ structural/Adapter/AdapterPatternExample.java
@@ -0,0 +1,34 @@
+interface AmericanSocket {
+        String provideElectricity();
+}
+
+
+class EuropeanPlug {
+        public String plugIn() {
+                return "European plug plugged in with 230V";
+        }
+}
+
+class AdapterAmericanToEuropean implements AmericanSocket {
+        private EuropeanPlug europeanPlug;
+
+        public AdapterAmericanToEuropean(EuropeanPlug europeanPlug) {
+                this.europeanPlug = europeanPlug;
+        }
+
+        @Override
+        public String provideElectricity() {
+                return convertVoltage() + " - " + europeanPlug.plugIn();
+        }
+
+        private String convertVoltage() {
+                return "Converting 120V from American Socket to 230V";
+        }
+}
+
+public class AdapterPatternExample {
+        public static void main(String[] args) {
+                AmericanSocket americanSocket = new AdapterAmericanToEuropean(new EuropeanPlug());
+                System.out.println(americanSocket.provideElectricity());
+        }
+}
